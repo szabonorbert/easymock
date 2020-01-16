c609311c672a1c49d9a77c087d8fc06c6f3646b2
unknown <szabonorbert@detenyleg.com>
2020-01-16 17:15:47 +0100

    testListBooksByAuthor tests

--- src/test/java/hu/szabonorbert/easymock/service/SalesmanTest.java
+++ src/test/java/hu/szabonorbert/easymock/service/SalesmanTest.java
@@ -32,7 +32,42 @@ public class SalesmanTest {
         dummyDB.add(new Book("Rejtő Jenő", "Az elveszett cirkáló", 1938));
         EasyMock.expect(bookshelf.listBooks()).andReturn(dummyDB);
         EasyMock.replay(bookshelf);
-
         Assert.assertEquals("Book{author='Rejtő Jenő', title='Az elveszett cirkáló', publishDate=1938}\n", salesman.listBooks());
+    }
+
+    @Test
+    public void testListBooksByAuthor(){
+        LinkedList<Book> dummyDB = new LinkedList();
+        dummyDB.add(new Book("Rejtő Jenő", "Az elveszett cirkáló", 1938));
+        dummyDB.add(new Book("Rejtő Jenő", "Vesztegzár a Grand Hotelben", 1940));
+        dummyDB.add(new Book("Mihail Csehov", "A színészhez", 1997));
+        EasyMock.expect(bookshelf.listBooks()).andReturn(dummyDB);
+        EasyMock.replay(bookshelf);
+
+        Assert.assertEquals("Book{author='Rejtő Jenő', title='Az elveszett cirkáló', publishDate=1938}\nBook{author='Rejtő Jenő', title='Vesztegzár a Grand Hotelben', publishDate=1940}\n", salesman.listBooksByAuthor("Rejtő Jenő"));
+    }
+
+    @Test
+    public void testListBooksByAuthor2(){
+        LinkedList<Book> dummyDB = new LinkedList();
+        dummyDB.add(new Book("Rejtő Jenő", "Az elveszett cirkáló", 1938));
+        dummyDB.add(new Book("Rejtő Jenő", "Vesztegzár a Grand Hotelben", 1940));
+        dummyDB.add(new Book("Mihail Csehov", "A színészhez", 1997));
+        EasyMock.expect(bookshelf.listBooks()).andReturn(dummyDB);
+        EasyMock.replay(bookshelf);
+
+        Assert.assertEquals("Book{author='Mihail Csehov', title='A színészhez', publishDate=1997}\n", salesman.listBooksByAuthor("Mihail Csehov"));
+    }
+
+    @Test
+    public void testListBooksByAuthor3(){
+        LinkedList<Book> dummyDB = new LinkedList();
+        dummyDB.add(new Book("Rejtő Jenő", "Az elveszett cirkáló", 1938));
+        dummyDB.add(new Book("Rejtő Jenő", "Vesztegzár a Grand Hotelben", 1940));
+        dummyDB.add(new Book("Mihail Csehov", "A színészhez", 1997));
+        EasyMock.expect(bookshelf.listBooks()).andReturn(dummyDB);
+        EasyMock.replay(bookshelf);
+
+        Assert.assertEquals("", salesman.listBooksByAuthor("J.K. Rowling"));
     }
 }
